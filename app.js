59045e9558ed6ee511ba41fc6d7a7b993c15784b
LeeSohyeon <teabag759@gmail.com>
2023-10-10 16:26:41 +0900

    ...

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const HIDEEN_CLASSNAME = "hidden";
-    // string만 포함된 변수는 대문자로 표기함(일종의 관습)
 
+// string만 포함된 변수는 대문자로 표기함(일종의 관습)
+const HIDEEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDEEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username)
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(savedUsername);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDEEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+//console.log(savedUsername);
+ 
+if(savedUsername === null) { 
+    loginForm.classList.remove(HIDEEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
